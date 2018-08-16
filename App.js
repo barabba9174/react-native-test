ca4e7b534c7d0c171227d6b154da381a653f28b0
barabba9174 <barbara.schiavinato@gmail.com>
2018-08-16 13:41:11 +0100

    add

--- App.js
+++ App.js
@@ -1,59 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import { createStackNavigator } from 'react-navigation';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import Third from './pages/Third';
+import Second from './pages/Second';
+import Home from './pages/Home';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const App = createStackNavigator({
+  Home: { screen: Home },
+  Second: { screen: Second },
+  Third: { screen: Third },
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native! test4</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    backgroundColor: 'red',
-    flex: 2,
-    width: '100%',
-    textAlignVertical: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    flex: 1,
-    backgroundColor: '#00ff00',
-    textAlignVertical: 'center',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-});
+export default App;
